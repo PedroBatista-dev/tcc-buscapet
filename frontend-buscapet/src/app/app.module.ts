abafa9c46aded9755a9d7910a4ba141df270e506
PedroBatista-dev <phmbbatista@gmail.com>
2023-01-15 17:10:56 -0300

    alterando versao do angular para 13

--- frontend-buscapet/src/app/app.module.ts
+++ frontend-buscapet/src/app/app.module.ts
@@ -5,6 +5,14 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { NgxMaskModule, IConfig } from 'ngx-mask';
+
+const maskConfigFunction: () => Partial<IConfig> = () => {
+  return {
+    validation: false,
+  };
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,6 +21,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     CoreModule,
     AppRoutingModule,
     NgbModule,
+    NgxMaskModule.forRoot(maskConfigFunction)
   ],
   providers: [],
   bootstrap: [AppComponent]
