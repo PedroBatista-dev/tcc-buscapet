23eedcc88aeac95397f679f085ee875e9e0bedce
PedroBatista-dev <phmbbatista@gmail.com>
2023-01-21 13:23:08 -0300

    implementando modulo de racas

--- frontend-buscapet/src/app/pages/breeds/breed-form/breed-form.component.ts
+++ frontend-buscapet/src/app/pages/breeds/breed-form/breed-form.component.ts
@@ -2,8 +2,13 @@ import { Component, ElementRef, Injector, ViewChildren } from '@angular/core';
 import { FormControlName, Validators } from '@angular/forms';
 
 import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
+import { Specie } from '../../species/shared/specie.model';
+import { SpecieService } from '../../species/shared/specie.service';
 import { Breed } from '../shared/breed.model';
 import { BreedService } from '../shared/breed.service';
+
+import Swal from 'sweetalert2';
+import { debounceTime, distinctUntilChanged, filter, map, Observable, OperatorFunction } from 'rxjs';
 
 @Component({
   selector: 'app-breed-form',
@@ -15,20 +20,34 @@ export class BreedFormComponent extends BaseResourceFormComponent<Breed> {
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements!: ElementRef[];
 
   breed: Breed = new Breed();
+  species: Specie[] = [];
 
-  constructor(protected breedService: BreedService, protected override injector: Injector) {
+  constructor(protected breedService: BreedService, protected override injector: Injector, public specieService: SpecieService) {
     super(injector, new Breed(), breedService, Breed.fromJson);
   }
 
   ngAfterViewInit(): void {
+    this.specieService.getAll('').subscribe({
+      next: (resources) => this.species = resources,
+      error: () => Swal.fire({
+              title: 'Erro!',
+              text: 'Erro ao buscar as espécies.',
+              icon: 'error',
+              confirmButtonColor: '#44C5CD',
+        })
+    });
     super.validarFormulario(this.formInputElements);
   }
 
   protected buildResourceForm(): void {
     this.resourceForm = this.formBuilder.group({
       name: [null, [Validators.required, Validators.minLength(2)]],
-      specie_id: [null, [Validators.required]]
+      specie: [null, [Validators.required]]
     });
+  }
+
+  printForm() {
+    console.log(this.resourceForm.value)
   }
 
   protected override creationPageTitle(): string {
@@ -39,4 +58,18 @@ export class BreedFormComponent extends BaseResourceFormComponent<Breed> {
     const breedName = this.resource.name || "";
     return `Editando Raça: ${breedName}`;
   }
+
+  search: OperatorFunction<string, Specie[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(200),
+      distinctUntilChanged(),
+      filter((term) => term.length >= 2),
+			map((term) =>
+				term === ''
+					? []
+					: this.species.filter((specie) => new RegExp(term, 'mi').test(specie.name!)).slice(0, 10)),
+		);
+
+  formatter = (specie: Specie) => specie.name!;
+
 }
