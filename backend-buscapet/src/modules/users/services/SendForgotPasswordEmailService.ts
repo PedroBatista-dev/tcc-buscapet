1f69d167a86512ce5be6e5c0930fa3bf2a8efbe9
PedroBatista-dev <phmbbatista@gmail.com>
2023-03-01 23:14:53 -0300

    configurando envio de email na aws ses

--- backend-buscapet/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend-buscapet/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,8 @@
 import AppError from '../../../shared/errors/AppError';
 import path from 'path';
 import EtherealMail from '../../../config/mail/EtherealMail';
+import SESMail from '../../../config/mail/SESMail';
+import mailConfig from '../../../config/mail/mail';
 import { IUsersRepository } from '../domain/repositories/IUsersRepository';
 import { IUserTokensRepository } from '../domain/repositories/IUserTokensRepository';
 import { inject, injectable } from 'tsyringe';
@@ -32,6 +34,24 @@ class SendForgotPasswordEmailService {
       'views',
       'forgot_password.hbs',
     );
+
+    if (mailConfig.driver === 'ses') {
+      await SESMail.sendMail({
+        to: {
+          name: user.name,
+          email: user.email,
+        },
+        subject: '[BuscaPet] Recuperação de Senha',
+        templateData: {
+          file: forgotPasswordTemplate,
+          variables: {
+            name: user.name,
+            link: `${process.env.APP_WEB_URL}/users/novo/resetar_senha?token=${token}`,
+          },
+        },
+      });
+      return;
+    }
 
     await EtherealMail.sendMail({
       to: {
